956c9a84abfd33864a7c5b0cd0f01bfbb42a6e5d
shubham lathiya <shubhamlathiya2004@gmail.com>
2025-09-29 16:44:58 +0530

    1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js app!');
+  res.send('Hello from Node.js shubham app!');
 });
 
 app.listen(PORT, () => {
