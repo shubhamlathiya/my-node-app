97f7ef77f3daba971309ec89bab101f6d040ca51
shubham lathiya <shubhamlathiya2004@gmail.com>
2025-09-30 17:06:28 +0530

    3

--- db.js
+++ db.js
@@ -1,19 +1,23 @@
+// db.js
 import mongoose from "mongoose";
 
 let cached = global.mongoose;
 
 if (!cached) cached = global.mongoose = { conn: null, promise: null };
 
-export default async function connectDB() {
+async function connectDB() {
     if (cached.conn) return cached.conn;
 
     if (!cached.promise) {
         cached.promise = mongoose.connect(process.env.MONGO_URI, {
+            bufferCommands: false,
             useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }).then((mongoose) => mongoose);
+            useUnifiedTopology: true
+        }).then(mongoose => mongoose);
     }
 
     cached.conn = await cached.promise;
     return cached.conn;
 }
+
+export default connectDB;
